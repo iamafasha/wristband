4dbf8396218f6bcb15451b6ab5527ff62e4e8b6e
cenit <blesstouch4@gmail.com>
2022-11-20 12:45:58 +0300

    from POST to GET

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 //get device data
-Route::post('/device/{device}', [Devices::class, 'getDeviceData']);
+Route::get('/device/{device}', [Devices::class, 'getDeviceData']);
