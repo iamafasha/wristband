688e4b64af3a5805b727e269c3a8f944cde634f8
iamafasha <afashaisakiye@gmail.com>
2022-11-23 22:38:11 +0300

    Add graph

--- resources/js/Pages/Patients/Show.vue
+++ resources/js/Pages/Patients/Show.vue
@@ -3,10 +3,13 @@ import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
 import { Head, useForm } from '@inertiajs/inertia-vue3';
 import BreezeLink from '@/Components/Link.vue';
 import BreezeButton from '@/Components/Button.vue';
+import PatientGraph from '@/Containers/PatientGraph.vue';
+import { reactive } from 'vue';
 
-const props = defineProps(['patient']);
+const props = defineProps(['patient', 'latest_reading']);
 const form = useForm({terms: false,});
 
+const data = reactive([["2000-06-05", 116], ["2000-06-06", 129], ["2000-06-07", 135], ["2000-06-08", 86], ["2000-06-09", 73], ["2000-06-10", 85], ["2000-06-11", 73], ["2000-06-12", 68], ["2000-06-13", 92], ["2000-06-14", 130], ["2000-06-15", 245], ["2000-06-16", 139], ["2000-06-17", 115], ["2000-06-18", 111], ["2000-06-19", 309], ["2000-06-20", 206], ["2000-06-21", 137], ["2000-06-22", 128], ["2000-06-23", 85], ["2000-06-24", 94], ["2000-06-25", 71], ["2000-06-26", 106], ["2000-06-27", 84], ["2000-06-28", 93], ["2000-06-29", 85], ["2000-06-30", 73], ["2000-07-01", 83], ["2000-07-02", 125], ["2000-07-03", 107], ["2000-07-04", 82], ["2000-07-05", 44], ["2000-07-06", 72], ["2000-07-07", 106], ["2000-07-08", 107], ["2000-07-09", 66], ["2000-07-10", 91], ["2000-07-11", 92], ["2000-07-12", 113], ["2000-07-13", 107], ["2000-07-14", 131], ["2000-07-15", 111], ["2000-07-16", 64], ["2000-07-17", 69], ["2000-07-18", 88], ["2000-07-19", 77], ["2000-07-20", 83], ["2000-07-21", 111], ["2000-07-22", 57], ["2000-07-23", 55], ["2000-07-24", 60]])
 </script>
 
 <template>
@@ -17,7 +20,8 @@ const form = useForm({terms: false,});
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="">
                     <div class="flex justify-between">
-                        <div class=" w-4/12 p-6 shadow-sm sm:rounded-lg bg-white border-b border-gray-200 h-fit">
+                        <div class="w-4/12" >
+                        <div class="p-6 shadow-sm sm:rounded-lg bg-white border-b border-gray-200 h-fit">
                             <!-- User data -->
                             <div class="flex flex-col justify-center items-center">
                                 <div class="flex justify-center">
@@ -67,7 +71,10 @@ const form = useForm({terms: false,});
                            </div>
                         </div>
                         <div class="w-8/12 pl-4">
-                            <div class="p-6 w-full shadow-sm sm:rounded-lg bg-white border-b border-gray-200">
+                            <div class="w-full overflow-hidden shadow-sm sm:rounded-lg bg-white border-b border-gray-200">
+                                <PatientGraph :data="data" />
+                            </div>
+                            <div class="p-6 w-full shadow-sm sm:rounded-lg bg-white border-b border-gray-200 mt-5">
                                 <h2>Devices</h2>
 
                                 <div class="pt-5">
